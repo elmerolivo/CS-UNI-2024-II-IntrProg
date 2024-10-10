4271b80f000f2d4af8171d0d321297435c7f473a
ecuadros <ecuadros@spc.org.pe>
2024-10-10 13:27:50 -0500

    Pointers full

--- pointers.cpp
+++ pointers.cpp
@@ -2,7 +2,69 @@
 #include "pointers.h"
 using namespace std;
 
+// Se envia copia de la variable. 
+// Paso por valor de un int
+void f1(int n){
+    ++n;
+}
+
+void f2(int &n){
+    ++n;
+}
+
+void f3(int *pi){
+    ++*pi;
+    pi = nullptr;
+}
+
+void f4(int *&rp){
+    ++*rp;
+    rp = nullptr;
+}
+
 void Pointers(){
-    cout << "Hello from Pointers\n";
-    
+    int x = 5, y = 10, &r = x;
+    int *p = nullptr, *q = nullptr;
+    int **pp = nullptr;
+
+    p = &x;     q = &y;
+    pp = &p;
+
+    cout << "x  está en: " << &x << endl;
+    cout << "y  está en: " << &y << endl;
+    cout << "p  está en: " << &p << endl;
+    cout << "q  está en: " << &q << endl;
+    cout << "pp está en: " << &pp << endl;
+
+    f1(x);  cout << "x=" << x << endl;
+    f1(7);
+    f1(r);  cout << "x=" << x << endl;
+
+    x = 5;
+    cout << "f2(int &n)\n";
+    f2(x);      cout << "x=" << x << endl;
+    f2(r);      cout << "x=" << x << endl;
+    f2(*p);     cout << "x=" << x << endl;
+    f2(**pp);   cout << "x=" << x << endl;
+    f2(*&x);    cout << "x=" << x << endl;
+    // f2(7);   error
+
+    x = 5;
+    cout << "f3(int *pi)\n";
+    f3(&x);     cout << "x=" << x << endl;
+    f3(p);      cout << "x=" << x << endl;
+    f3(p);      cout << "x=" << x << endl;
+    f3(&r);     cout << "x=" << x << endl;
+    f3(*pp);    cout << "x=" << x << endl;
+    // Error f3(nullptr);
+
+    x = 5;
+    cout << "f4(int *&pi)\n";
+    // Error f4(&x);
+    f4(p);      cout << "x=" << x << endl;
+    p = &x;
+    f4(*pp);    cout << "x=" << x << endl;
+    cout << "p=" << p <<endl;
+    f4(q);      cout << "y=" << y << endl;
+                cout << "q=" << q << endl;
 }
--- pointers.h
+++ pointers.h
@@ -1,6 +1,11 @@
 #ifndef __POINTERS_H__
 #define __POINTERS_H__
 
+void f1(int n);
+void f2(int &n);
+void f3(int *pi);
+void f4(int *&rp);
+
 void Pointers();
 
 #endif // __POINTERS_H__
