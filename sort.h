9a6bc0dfc6ce4aa1ac1864184a9b43944fcd4363
ecuadros <ecuadros@spc.org.pe>
2024-11-05 15:22:19 -0500

    Sort con problemas

--- sort.h
+++ sort.h
@@ -4,31 +4,31 @@
 #include <algorithm>    // std::swap
 using namespace std;
 
-// Función para particionar el arreglo
+// Función para partition el arreglo
 template <typename T>
-size_t particionar(T arr[], size_t bajo, size_t alto) {
-    T pivote = arr[alto]; // Elegimos el último elemento como pivote
-    size_t i = bajo - 1; // Índice del elemento más pequeño
+size_t partition(T arr[], size_t low, size_t high) {
+    T pivote = arr[high]; // Elegimos el último elemento como pivote
+    size_t i = low - 1; // Índice del elemento más pequeño
 
-    for (size_t j = bajo; j < alto; j++) {
-        if (arr[j] <= pivote) {
-            i++; // Incrementar el índice del elemento más pequeño
+    for (size_t j = low; j < high; j++) {
+        if ( arr[j] >= pivote ) {
+            i++;
             swap(arr[i], arr[j]); // Intercambiar
         }
     }
-    swap(arr[i + 1], arr[alto]); // Colocar el pivote en la posición correcta
+    swap(arr[i + 1], arr[high]); // Colocar el pivote en la posición correcta
     return i + 1; // Retornar el índice del pivote
 }
 
 // Función recursiva para aplicar QuickSort
 template <typename T>
-void QuickSort(T arr[], size_t bajo, size_t alto) {
-    if (bajo < alto) {
-        size_t pi = particionar(arr, bajo, alto); // Particionar el arreglo
+void QuickSort(T arr[], size_t low, size_t high) {
+    if (low < high) {
+        size_t pi = partition(arr, low, high); // partition el arreglo
 
         // Ordenar recursivamente los elementos antes y después de la partición
-        QuickSort(arr, bajo, pi - 1);
-        QuickSort(arr, pi + 1, alto);
+        QuickSort(arr, low, pi - 1);
+        QuickSort(arr, pi + 1, high);
     }
 }
 
