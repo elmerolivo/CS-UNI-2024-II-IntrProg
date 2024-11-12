5599fd98e7d87b6a960542c90c52a22fbf0e8242
ecuadros <ecuadros@spc.org.pe>
2024-11-12 13:08:00 -0500

    Funcion de comparacion flexible

--- sort.h
+++ sort.h
@@ -5,16 +5,26 @@
 using namespace std;
 
 template <typename T>
-int partition(T arr[], int low, int high) {
-    T pivot = arr[low]; // Pivote como el primer elemento
-    int i = low + 1;
-    int j = high;
+bool men(T a, T b)
+{
+   return a>b;
+}
+
+template <typename T>
+bool may(T a, T b)
+{
+   return a<b;
+}
+
+template <typename T>
+int partition(T arr[], int low, int high, bool (*pfc)(T, T)) {
+    auto pivot = arr[low]; // Pivote como el primer elemento
+    auto i = low + 1;
+    auto j = high;
 
     while (true) {
-        // Por la izq deben estar los menores 
-        while (i <= j && arr[i] <= pivot)   i++;
-        // Por la der deben estar los mayores
-        while (i <= j && arr[j] > pivot)    j--;
+        while (i <= j && !(*pfc)(arr[i], pivot))   i++;
+        while (i <= j && (*pfc)(arr[j], pivot))    j--;
         // Se cruzaron los índices, salir del bucle
         if (i > j)  break; 
         std::swap(arr[i], arr[j]);  // Intercambiar elementos
@@ -25,11 +35,11 @@ int partition(T arr[], int low, int high) {
 }
 
 template <typename T>
-void QuickSort(T arr[], int low, int high) {
+void QuickSort(T arr[], int low, int high, bool (*pfc)(T, T)) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        QuickSort(arr, low, pi -1);
-        QuickSort(arr, pi + 1, high);
+        int pi = partition(arr, low, high, pfc);
+        QuickSort(arr, low, pi -1, pfc);
+        QuickSort(arr, pi + 1, high, pfc);
     }
 }
 
